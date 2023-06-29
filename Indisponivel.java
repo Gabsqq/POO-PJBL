5de397d10af223b46ff3027e3e0bc1bccb78ba77
Gabsqq <85641230+Gabsqq@users.noreply.github.com>
2023-06-29 12:24:30 -0300

    Delete Indisponivel.java

--- Indisponivel.java
+++
@@ -1,7 +0,0 @@
-package Biblioteca;
-
-public class Indisponivel extends Exception {
-    public Indisponivel() {
-        super("Pesquisas não estão disponíveis para empréstimos, apenas para uso na biblioteca");
-    }
-}
